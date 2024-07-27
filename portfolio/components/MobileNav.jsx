9345318b980ae5538357d86dc6909c2ec1be4452
Zeus <mwesigyejosephug@gmail.com>
2024-07-27 17:43:19 +0300

    Added Socials and Image

--- portfolio/components/MobileNav.jsx
+++ portfolio/components/MobileNav.jsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Link } from "lucide-react";
 import { CiMenuFries } from "react-icons/ci";
 
 const links = [
@@ -37,7 +37,31 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         {/* logo */}
-        <div>logo</div>
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Mwesigye <span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+
+        {/* nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
       </SheetContent>
     </Sheet>
   );
